6a2a5a2f25e908393d240003160f3099c42e292b
dittrmdhn <adityaramadhan63.ar@gmail.com>
2024-06-01 09:57:40 +0700

    update domain and title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import Educations from "./components/Educations";
 
@@ -17,7 +15,6 @@ function App() {
 			<Skills />
 			<Projects />
 			<Educations />
-			{/* <Contact /> */}
 			<Footer />
 		</div>
 	);
